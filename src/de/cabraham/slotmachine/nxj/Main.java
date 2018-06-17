1267aa01e97296ef8d4afe6bf37658bac1d98c08
starfish0r@gmail.com <starfish0r@gmail.com>
2018-06-17 22:56:48 +0200

    RConsole logging

--- src/de/cabraham/slotmachine/nxj/Main.java
+++ src/de/cabraham/slotmachine/nxj/Main.java
@@ -2,15 +2,21 @@ package de.cabraham.slotmachine.nxj;
 
 import java.io.OutputStream;
 
+import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Sound;
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.RConsole;
 
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
+		RConsole.openUSB(10000);
+	    RConsole.println("wat");
+	    Button.waitForAnyPress();
+	    RConsole.println("wat2");
 		BTConnection conn = null;
 		int cnt = 0;
 		try {
@@ -20,17 +26,25 @@ public class Main {
 				LCD.drawString(String.valueOf(conn), 2, 2);
 				cnt++;
 			}
-			Sound.beep();
+			//Sound.beep();
 			//conn.setIOMode(NXTConnection.RAW);
 			LCD.drawString("OK", 0, 0);
 			LCD.refresh();
+			RConsole.println(conn.getAddress());
 			OutputStream openOutputStream = conn.openOutputStream();
-			openOutputStream.write(1);
+			for(int i=0;i<5;i++) {
+				openOutputStream.write(1);
+				Thread.sleep(1000);
+			}
 			openOutputStream.flush();
 			openOutputStream.close();
-		}catch(Exception ex){
+		} catch(Throwable ex){
 			LCD.drawString(ex.getMessage(), 0, 15);
+			ex.printStackTrace();
+			RConsole.println(ex.getMessage());
 		}
+		
+	    RConsole.close();
 		Sound.beep();
 	}
 
